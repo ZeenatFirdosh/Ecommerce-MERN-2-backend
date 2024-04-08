510cf649ccf2e18b18381f9af6820322f3391a11
Zeenat <zeenatzfq052@gmail.com>
2024-04-08 23:37:10 +0530

    cors issue

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-require('dotenv').config()
-const connectDB = require('./config/db')
-const router = require('./routes')
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const connectDB = require("./config/db");
+const router = require("./routes");
 
+const app = express();
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+// app.use(cors({
+//     origin : [process.env.FRONTEND_URL,"http://localhost:8080"],
+//     credentials : true
+// }))
+// app.use(cors());
 
-const app = express()
-app.use(cors({
-    origin : [process.env.FRONTEND_URL,"http://localhost:8080"],
-    credentials : true
-}))
-app.use(express.json())
-app.use(cookieParser())
+app.use(allowCrossDomain);
 
-app.use("/api",router)
+app.use(express.json());
+app.use(cookieParser());
 
-const PORT = 8080 || process.env.PORT
+app.use("/api", router);
 
+const PORT = 8080 || process.env.PORT;
 
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("connnect to DB")
-        console.log("Server is running "+PORT)
-    })
-})
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("connnect to DB");
+    console.log("Server is running " + PORT);
+  });
+});
